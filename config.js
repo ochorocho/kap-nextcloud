d3a957d88c45a762fa74de97db46dbea44e5d042
Jochen Roth <j.roth@zeroseven.de>
2019-02-02 12:52:02 +0100

    Add configurations window, save config values, catch share link error

--- config.js
+++ config.js
@@ -1,24 +1,37 @@
 const electron = require('electron');
-const querystring = require('querystring');
-const url = require('url');
+const {ipcMain} = require('electron')
 
 const BrowserWindow = electron.BrowserWindow || electron.remote.BrowserWindow;
 
 const getConfig = context => {
 
-  return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
 
-    let configWindow = new BrowserWindow({width:400, height:400})
-    configWindow.loadURL(`file://${__dirname}/` + `config.html`)
+        let configWindow = new BrowserWindow({
+            width: 400,
+            height: 440,
+            alwaysOnTop: true
+        });
+        configWindow.loadURL(`file://${__dirname}/` + 'config.html')
 
-    configWindow.on('closed', () => {
-      if (!gotCode) {
-        reject(new Error('canceled'));
-      }
+        let configValues = {
+            url: context.config.get("url"),
+            username: context.config.get("username"),
+            password: context.config.get("password"),
+            path: context.config.get("path")
+        };
+        configWindow.config = configValues;
+
+        ipcMain.on('asynchronous-message', (event, arg) => {
+            for (key in arg) {
+                context.config.set(key, arg[key])
+            }
+        });
+
+        configWindow.on('closed', () => {
+            resolve()
+        });
     });
-
-
-  });
-}
+};
 
 module.exports = getConfig;
--- index.js
+++ index.js
@@ -4,34 +4,23 @@ const path = require('path');
 const urljoin = require('url-join');
 const request = require('request');
 const moment = require('moment');
-const {dialog} = require('electron');
 const getConfig = require('./config');
 
-const err = message => dialog.showMessageBox({message, type: "error"});
-
 const action = async context => {
+    /**
+     * When to open auth window
+     */
+    if (context.config.get('url') == "" || context.config.get('username') == "" || context.config.get('password') == "" || context.config.get('path') == "") {
+        context.setProgress('Authorizing…');
+        await getConfig(context);
+    }
+
     const filePath = await context.filePath();
     const filename = path.basename(filePath);
     const fileTarget = urljoin(context.config.get('path') + filename);
     const validUntil = moment().add(7, 'days').format('YYYY-MM-DD');
     const uploadUrl = urljoin(context.config.get('url'), '/remote.php/webdav/', fileTarget);
     const shareUrl = urljoin(context.config.get('url'),'/ocs/v1.php/apps/files_sharing/api/v1/shares');
-
-
-    context.config.set('username', await getConfig(context));
-    if (context.config.has('url') == null || context.config.has('username') == null || context.config.has('password') == null || context.config.has('path') == null) {
-        context.setProgress('Authorizing…');
-        try {
-            context.config.set('username', await getConfig(context));
-        } catch (error) {
-            if (error.message === 'canceled') {
-                context.cancel();
-            } else {
-                err(error.message);
-            }
-            return;
-        }
-    }
 
     /**
      * Create Read Stream of filePath
@@ -61,7 +50,6 @@ const action = async context => {
             if (error) {
                 return console.error('Nextcloud upload failed:', error);
             }
-            console.log("Upload done ...");
         });
 
     /**
@@ -87,8 +75,12 @@ const action = async context => {
             }
         },
         function (error, response, body) {
-            context.copyToClipboard(body.ocs.data.url);
-            context.notify('Nextcloud uploaded, link copied to Clipboard');
+            try {
+                context.copyToClipboard(body.ocs.data.url);
+                context.notify('Nextcloud uploaded, link copied to Clipboard');
+            } catch (e) {
+                context.notify("Sorry mate! Could not get share link");
+            }
         });
 };
 
